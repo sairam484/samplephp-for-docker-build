214800be1c04f9372bba1fec6fe1b1725420f59e
sairam g <88833080+sairam484@users.noreply.github.com>
2021-11-11 19:14:28 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello php umashankar dada mbbs</p>'; ?> 
+ <?php echo '<p>Hello php umashankar dada mbbs abhinav is scolding me</p>'; ?> 
  </body>
 </html>  
